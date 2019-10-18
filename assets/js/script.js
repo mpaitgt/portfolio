fba004309627a521f07398f7db9fe5da59aaf274
Matt Pignatore <mattpigs@gmail.com>
2019-10-17 21:14:03 -0400

    used css grid to properly style my work examples. added social icons and used grid to position them

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,30 @@
 var skillBtn = document.getElementById('skill-btn');
+var pickedSkill = document.getElementById('random-skill');
 var skillsArray = ['JavaScript', 'jQuery', 'HTML/CSS', 'Bootstrap', 'Firebase', 'Adobe Suite'];
 
-skillBtn.addEventListener('click', function() {
+skillBtn.addEventListener('mouseup', function() {
     randomSkill(skillsArray)
 });
 
+skillBtn.addEventListener('mousedown', function() {
+    pickedSkill.removeAttribute('class');
+});
+
+// document.onload = function() {
+//     body.setAttribute('class', 'onload');
+// }
+
 function randomSkill(array) {
-    var randomSkill = document.getElementById('random-skill');
-    randomSkill.innerHTML = '';
-    var skill = array[Math.floor(Math.random() * array.length)];
-    var skillNode = document.createTextNode(skill);
-    randomSkill.appendChild(skillNode);
-    console.log(skill);
+    if (array.length === 0) {
+        pickedSkill.style.fontSize = '72px';
+        pickedSkill.innerHTML = 'Scroll down to see my work';
+    } else {
+        pickedSkill.innerHTML = '';
+        pickedIndex = Math.floor(Math.random() * array.length)
+        var skill = array[pickedIndex];
+        var skillNode = document.createTextNode(skill);
+        pickedSkill.appendChild(skillNode);
+        pickedSkill.setAttribute('class', 'enter');
+        array.splice(pickedIndex, 1);
+    }
 }
