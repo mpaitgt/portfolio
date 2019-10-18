4cf2cdb2ba2bb7ee1e698c1367bdf3d95812c8fc
Matt Pignatore <mattpigs@gmail.com>
2019-10-18 19:38:26 -0400

    added an interests button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,30 +1,76 @@
+// GLOBAL VARIABLES
 var skillBtn = document.getElementById('skill-btn');
-var pickedSkill = document.getElementById('random-skill');
+var pickedCat = document.getElementById('random-category');
 var skillsArray = ['JavaScript', 'jQuery', 'HTML/CSS', 'Bootstrap', 'Firebase', 'Adobe Suite'];
+var interestsArray = ['Gluten-Free Baking', 'Songwriting / Producing', 'Film / Television', 'Home Cooking']
 
-skillBtn.addEventListener('mouseup', function() {
-    randomSkill(skillsArray)
+
+// EVENT LISTENERS
+document.querySelectorAll('.button').forEach(function(buttonSelect) {
+    buttonSelect.addEventListener('mouseup', function() {
+        
+        if (this.innerText === 'Skill') {
+            randomSkill(skillsArray);
+        } else {
+            randomInterest(interestsArray);
+        }        
+    });
 });
 
-skillBtn.addEventListener('mousedown', function() {
-    pickedSkill.removeAttribute('class');
+document.querySelectorAll('.button').forEach(function(buttonSelect) {
+    buttonSelect.addEventListener('mousedown', function() {
+        pickedCat.removeAttribute('class');
+    });
 });
 
-// document.onload = function() {
-//     body.setAttribute('class', 'onload');
-// }
+document.querySelectorAll('.hover-function').forEach(function(dataClass) {
+    dataClass.addEventListener('mouseover', displayLink)
+});
+
+
+// FUNCTIONS
+function displayLink() {
+    var linkBox = document.getElementById('link-box');
+    var newLink = document.createElement('h1');
+    var newHREF = this.getAttribute('href');
+    console.log(this);
+    console.log(newHREF);
+
+    while (linkBox.firstChild) {
+        linkBox.removeChild(linkBox.firstChild);
+        }
+
+    newLink.innerText = newHREF;
+    newLink.setAttribute('class', 'hover-link enter');
+    linkBox.appendChild(newLink);    
+}
 
 function randomSkill(array) {
     if (array.length === 0) {
-        pickedSkill.style.fontSize = '72px';
-        pickedSkill.innerHTML = 'Scroll down to see my work';
+        pickedCat.style.fontSize = '72px';
+        pickedCat.innerHTML = 'Scroll down to see my work';
     } else {
-        pickedSkill.innerHTML = '';
+        pickedCat.innerHTML = '';
         pickedIndex = Math.floor(Math.random() * array.length)
         var skill = array[pickedIndex];
         var skillNode = document.createTextNode(skill);
-        pickedSkill.appendChild(skillNode);
-        pickedSkill.setAttribute('class', 'enter');
+        pickedCat.appendChild(skillNode);
+        pickedCat.setAttribute('class', 'enter');
+        array.splice(pickedIndex, 1);
+    }
+}
+
+function randomInterest(array) {
+    if (array.length === 0) {
+        pickedCat.style.fontSize = '72px';
+        pickedCat.innerHTML = 'Scroll down to see my what more I do';
+    } else {
+        pickedCat.innerHTML = '';
+        pickedIndex = Math.floor(Math.random() * array.length)
+        var interest = array[pickedIndex];
+        var catNode = document.createTextNode(interest);
+        pickedCat.appendChild(catNode);
+        pickedCat.setAttribute('class', 'enter');
         array.splice(pickedIndex, 1);
     }
 }
