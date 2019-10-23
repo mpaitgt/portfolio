b3eb1fb9534575d161bb8259133fcb162f6dda83
Matt Pignatore <mattpigs@gmail.com>
2019-10-23 13:11:51 -0400

    mobile-responsiveness

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,4 @@
-// GLOBAL VARIABLES
-var skillBtn = document.getElementById('skill-btn');
-var pickedCat = document.getElementById('random-category');
-var skillsArray = ['JavaScript/jQuery', 'CSS/Bootstrap', 'Firebase', 'Adobe Creative Suite'];
-    
 // EVENT LISTENERS
-document.querySelectorAll('.button').forEach(function(buttonSelect) {
-    buttonSelect.addEventListener('mouseup', function() {
-            showArray(skillsArray);
-    });
-});
-
-document.querySelectorAll('.button').forEach(function(buttonSelect) {
-    buttonSelect.addEventListener('mousedown', function() {
-    
-    });
-});
-
-
-// FUNCTIONS
-function showArray(array) {
-    pickedCat = $('#random-category');
-    for (var x = 0; x < array.length; x++) {
-        var listItem = $('<p>');
-        listItem.text(array[x]);
-        listItem.animate({
-            opacity: '1'
-        })
-        pickedCat.append(listItem);
-    }
-}
-
 $("#about-link").click(function() {
     $([document.documentElement, document.body]).animate({
         scrollTop: $("#home-icons").offset().top
@@ -52,5 +21,11 @@ $("#contact-link").click(function() {
     $('#home-icons').animate({
         opacity: '1',
         bottom: '20px'
+    }, 500);
+});
+
+$("#top-link").click(function() {
+    $([document.documentElement, document.body]).animate({
+        scrollTop: $("#top-landing").offset().top
     }, 500);
 });
