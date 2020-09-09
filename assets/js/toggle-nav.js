8cea4c2f6e65632846b2c6e979e3aab2fdc0a9c3
Matt Pignatore <mattpigs@gmail.com>
2020-09-09 07:21:51 -0400

    continuing to work on media queries

--- assets/js/toggle-nav.js
+++ assets/js/toggle-nav.js
@@ -3,9 +3,10 @@ const
   navContents = document.querySelector('.nav__ul');
 
 burger.addEventListener('click', function(e) {
-  if (navContents.classList.contains('toggle-nav')) {
-    navContents.classList.remove('toggle-nav');
-  } else {
-    navContents.classList.add('toggle-nav');
-  }
+  // if (navContents.classList.contains('toggle-nav')) {
+  //   navContents.classList.remove('toggle-nav');
+  // } else {
+  //   navContents.classList.add('toggle-nav');
+  // }
+  navContents.classList.toggle('toggle-nav');
 })
