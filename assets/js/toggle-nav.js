23e3eeb171514498fb048a17ce18f0021a130080
Matt Pignatore <mattpigs@gmail.com>
2021-03-01 20:41:32 -0500

    Further refined mobile-friendly version of portfolio and cleaned up some of the JS files

--- assets/js/toggle-nav.js
+++ assets/js/toggle-nav.js
@@ -12,24 +12,3 @@ navItems.forEach(item => {
     navContents.classList.toggle('toggle-nav');
   })
 })
-// document.addEventListener('click', function(e) {
-//   const navItem = document.querySelectorAll('.nav__link');
-//   if (e.target === burger) {
-//     navContents.classList.toggle('toggle-nav');
-//   }
-// })
-
-function isDescendant(el, selector) {
-  let isChild = false;
-  if (el.id === selector) {
-    isChild = true;
-    return isChild;
-  }
-  while (el = el.parentNode) {
-    if (el.id === selector || $(el).parents('.select2-container').length == 1) {
-      isChild = true;
-      return isChild;
-    }
-  }
-  return isChild;
-}
