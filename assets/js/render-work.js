83ad687fa1fc5fabd7781e500c717bb952c2e07b
Matt Pignatore <mattpignatore@Matts-MacBook-Pro-2.local>
2022-01-05 15:37:55 -0500

    Fixed scroll

--- assets/js/render-work.js
+++ assets/js/render-work.js
@@ -115,7 +115,7 @@ function attachShowEvent() {
         item.classList.add('active');
         thisJob.scrollIntoView({
           behavior: 'smooth',
-          block: 'start'
+          block: 'center'
         }, 5000);
       }
     });
